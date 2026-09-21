0dc53ceb06a2b483c7d48549c99de7b1f8287e61
Thibaut Durand <durand.tibo@gmail.com>
2026-09-20 18:23:24 -0700

    Improve tests

--- tests/test_test.py
+++ tests/test_test.py
@@ -31,8 +31,10 @@ def test_all_with_coverage() -> None:
     c = _context({"package": {"name": "mypkg"}})
     test_tasks.all(c, cov=True)
     assert _commands(c) == [
-        "python -m pytest --xdoctest --timeout 10 "
-        "--cov-report html --cov-report xml --cov-report term --cov=mypkg tests"
+        (
+            "python -m pytest --xdoctest --timeout 10 "
+            "--cov-report html --cov-report xml --cov-report term --cov=mypkg tests"
+        )
     ]
 
 
@@ -46,8 +48,10 @@ def test_unit_with_coverage() -> None:
     c = _context({"package": {"name": "mypkg"}})
     test_tasks.unit(c, cov=True)
     assert _commands(c) == [
-        "python -m pytest --xdoctest --timeout 10 "
-        "--cov-report html --cov-report xml --cov-report term --cov=mypkg tests/unit"
+        (
+            "python -m pytest --xdoctest --timeout 10 "
+            "--cov-report html --cov-report xml --cov-report term --cov=mypkg tests/unit"
+        )
     ]
 
 
@@ -61,8 +65,10 @@ def test_integration_with_coverage() -> None:
     c = _context({"package": {"name": "mypkg"}})
     test_tasks.integration(c, cov=True)
     assert _commands(c) == [
-        "python -m pytest --xdoctest --timeout 60 --cov-report html --cov-report xml "
-        "--cov-report term --cov-append --cov=mypkg tests/integration"
+        (
+            "python -m pytest --xdoctest --timeout 60 --cov-report html --cov-report xml "
+            "--cov-report term --cov-append --cov=mypkg tests/integration"
+        )
     ]
 
 
@@ -76,8 +82,10 @@ def test_functional_with_coverage() -> None:
     c = _context({"package": {"name": "mypkg"}})
     test_tasks.functional(c, cov=True)
     assert _commands(c) == [
-        "python -m pytest --xdoctest --timeout 60 --cov-report html --cov-report xml "
-        "--cov-report term --cov-append --cov=mypkg tests/functional"
+        (
+            "python -m pytest --xdoctest --timeout 60 --cov-report html --cov-report xml "
+            "--cov-report term --cov-append --cov=mypkg tests/functional"
+        )
     ]
 
 
